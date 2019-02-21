9a8fc855096ecebb7343bfcc8061eb930daf272b
Yusuf Nafey <yusufnafey@yahoo.com>
2019-02-20 20:58:08 -0800

    added navigation bar

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React, { Component } from "react";
 import { Route } from "react-router-dom";
 import "./App.scss";
 import Home from "./Home";
+import Navigation from "./Navigation";
 
 class App extends Component {
   render() {
     return (
       <div className="app">
-        {/* <Navigation /> */}
+        <Navigation />
         <Route exact path="/" component={Home} />
         {/* <Route path="/about" component={About} />
         <Route path="/contact" component={Contact} /> */}
---
+++ src/Navigation.js
@@ -0,0 +1,34 @@
+import React from "react";
+import "./Navigation.scss";
+import { Link } from "react-router-dom";
+
+const Navigation = props => {
+  return (
+    <div className="navigation">
+      <div className="top">
+        <div className="container">
+          {/* <img
+            className="logo"
+            src={require("../images/isotLogo.png")}
+            alt="isot logo"
+          /> */}
+        </div>
+      </div>
+      <div className="bottom">
+        <div className="container">
+          <div className="navbar">
+            <Link to="/">Home</Link>
+            <Link to="/about">About</Link>
+            <Link to="/contact">Contact</Link>
+            <Link to="/donate">Donate</Link>
+            <Link to="/events">Events</Link>
+            <Link to="/prayer_times">Prayer Times</Link>
+            <Link to="/forms">Forms</Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navigation;
