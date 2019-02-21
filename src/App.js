7680a1186738de5c32fd1741461e0061cff6d230
Yusuf Nafey <yusufnafey@yahoo.com>
2019-02-20 20:55:06 -0800

    fixed errors, added home styling

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import React, { Component } from "react";
 import { Route } from "react-router-dom";
 import "./App.scss";
-import Navigation from "./Navigation/Navigation";
-import Home from "./Home/Home";
-import About from "./About/About";
-import Contact from "./Contact/Contact";
+import Home from "./Home";
 
 class App extends Component {
   render() {
     return (
       <div className="app">
-        <Navigation />
+        {/* <Navigation /> */}
         <Route exact path="/" component={Home} />
         {/* <Route path="/about" component={About} />
         <Route path="/contact" component={Contact} /> */}
