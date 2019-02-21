c5b0b0a08a9ec7318eac17888ba3083eaae275d4
Yusuf Nafey <yusufnafey@yahoo.com>
2019-02-20 19:57:30 -0800

    set up app structure

---
+++ src/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Home.scss";
+// import PrayerTime from "./PrayerTime/PrayerTime";
+
+const Home = props => {
+  return (
+    <div className="container">
+      {/* <PrayerTime /> */}
+      <div className="hi">hello</div>
+    </div>
+  );
+};
+
+export default Home;
