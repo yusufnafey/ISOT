3c1a2d6786ffe41b28e7e5f917fc7f1231d02600
Yusuf Nafey <yusufnafey@yahoo.com>
2019-02-21 23:03:14 -0800

    contact page

---
+++ src/Contact.js
@@ -0,0 +1,42 @@
+import React from "react";
+import "./Contact.scss";
+
+const Contact = props => {
+  return (
+    <div className="contact-page">
+      <div className="titles">Contact</div>
+      <div className="contact-info container">
+        <div className="logo-background">
+          <i class="fa fa-map-marker" aria-hidden="true" />
+        </div>
+        <span className="contact-text">
+          350 Corral Hollow Road <br /> Tracy, CA 95376
+        </span>
+        <div className="logo-background">
+          <i class="fa fa-phone" aria-hidden="true" />
+        </div>
+        <span className="contact-text">(209) 489-3367</span>
+        <div className="logo-background">
+          <i class="fa fa-envelope" aria-hidden="true" />
+        </div>
+        <span className="contact-text">musatarin@yahoo.com</span>
+        <div className="logo-background">
+          <i class="fa fa-globe" aria-hidden="true" />
+        </div>
+        <span className="contact-text">www.isotracy.org</span>
+        <iframe
+          width="500"
+          height="400"
+          id="gmap_canvas"
+          src="https://maps.google.com/maps?q=islamic%20society%20of%20tracy&t=&z=13&ie=UTF8&iwloc=&output=embed"
+          frameborder="0"
+          scrolling="no"
+          marginheight="0"
+          marginwidth="0"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
